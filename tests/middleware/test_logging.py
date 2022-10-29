daa1367713122e77836a8d9751edccab75b263cc
Irfanuddin <irfanudeen08@gmail.com>
2022-10-29 22:16:00 +0530

    add client to test

--- tests/middleware/test_logging.py
+++ tests/middleware/test_logging.py
@@ -220,6 +220,7 @@ async def test_default_logging_with_uds(
         assert "ASGI 'lifespan' protocol appears unsupported" in messages.pop(0)
         assert "Application startup complete" in messages.pop(0)
         assert "Uvicorn running on unix socket " + short_socket_name in messages.pop(0)
+        assert '"GET / HTTP/1.1" 204' in messages.pop(0)
         assert "Shutting down" in messages.pop(0)
 
 
