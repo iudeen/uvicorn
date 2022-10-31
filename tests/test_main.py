d27c2110344aee5fef5951016ae1b701efa5c53d
Irfanuddin <irfanudeen08@gmail.com>
2022-11-01 01:00:56 +0530

    add shortsokcetname

--- tests/test_main.py
+++ tests/test_main.py
@@ -138,7 +138,7 @@ async def test_run_multiprocess_with_sockets(caplog, short_socket_name):
     with caplog_for_logger(caplog, "uvicorn.access"):
         async with run_server(config, sockets=[sock]):
             await asyncio.sleep(1)
-            sock.bind(short_socket_name)
+            sock.connect(short_socket_name)
             sock.listen(9)
         messages = [
             record.message for record in caplog.records if "uvicorn" in record.name
