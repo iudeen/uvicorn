f9d5274e04c547ef758a1683847a0b17d687cd47
Irfanuddin <irfanudeen08@gmail.com>
2022-10-31 19:01:44 +0530

    remove pragma

--- uvicorn/server.py
+++ uvicorn/server.py
@@ -176,6 +176,7 @@ class Server:
 
         if config.fd is not None:
             sock = listeners[0]
+            logger.info(listeners)  # added for testing only-remove it!
             logger.info(
                 "Uvicorn running on socket %s (Press CTRL+C to quit)",
                 sock.getsockname(),
